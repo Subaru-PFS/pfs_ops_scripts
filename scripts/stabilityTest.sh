eba1a83b4bbebbae1b808fcc79828460ae3b16a2
Arnaud Le Fur <arnaud.lefur@lam.fr>
2021-01-17 19:50:09 +0100

    stabilityTest as name, add dcb pupil mask size as comments

--- scripts/stabilityTest.sh
+++ scripts/stabilityTest.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
+export COMMENTS=F/2.5
 
-oneCmd.py iic bias comments=stabilityTest
+oneCmd.py iic bias name=stabilityTest
 echo
-oneCmd.py iic dark exptime=300 comments=stabilityTest
+oneCmd.py iic dark exptime=300 name=stabilityTest
 echo
-oneCmd.py iic scienceTrace halogen=3.0 comments=stabilityTest
+oneCmd.py iic scienceTrace halogen=3.0 name=stabilityTest  comments=$COMMENTS
 echo
-oneCmd.py iic scienceArc argon=45 comments=stabilityTest
+oneCmd.py iic scienceArc argon=45 name=stabilityTest  comments=$COMMENTS
 echo
-oneCmd.py iic scienceArc neon=15 comments=stabilityTest
+oneCmd.py iic scienceArc neon=15 name=stabilityTest  comments=$COMMENTS
 echo
-oneCmd.py iic scienceArc krypton=45 comments=stabilityTest
+oneCmd.py iic scienceArc krypton=45 name=stabilityTest  comments=$COMMENTS
 echo
-oneCmd.py iic scienceArc hgar=5.0 comments=stabilityTest
+oneCmd.py iic scienceArc hgar=5.0 name=stabilityTest  comments=$COMMENTS
 echo
-
