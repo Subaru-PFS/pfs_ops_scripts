5b37d7f0b57c94ee5485feecac6c559de70b250c
Arnaud Le Fur <afur@astro.princeton.edu>
2023-02-15 12:08:51 -0500

    stabilityTest badly parsed

--- scripts/stabilityTest.sh
+++ scripts/stabilityTest.sh
@@ -3,26 +3,24 @@
 # user inputs
 export SPECNUM=3
 export NAME=stabilityTest
-export COMMENTS=
 
-
-oneCmd.py iic bias specNum=$SPECNUM name=$NAME comments=$COMMENTS
+oneCmd.py iic bias specNum=$SPECNUM name=$NAME
 echo
-oneCmd.py iic dark exptime=300 specNum=$SPECNUM name=$NAME comments=$COMMENTS
+oneCmd.py iic dark exptime=300 specNum=$SPECNUM name=$NAME
 echo
-oneCmd.py iic scienceTrace halogen=30 arm=b specNum=$SPECNUM name=$NAME comments=$COMMENTS
-echo
-oneCmd.py iic bias specNum=$SPECNUM name=$NAME comments=$COMMENTS
-echo
-oneCmd.py iic scienceTrace halogen=3 arm=r specNum=$SPECNUM name=$NAME comments=$COMMENTS
-echo
-oneCmd.py iic scienceArc argon=4 specNum=$SPECNUM name=$NAME comments=$COMMENTS
-echo
-oneCmd.py iic scienceArc neon=3 specNum=$SPECNUM name=$NAME comments=$COMMENTS
-echo
-oneCmd.py iic scienceArc hgar=4 specNum=$SPECNUM name=$NAME comments=$COMMENTS
-echo
-oneCmd.py iic scienceArc krypton=60 specNum=$SPECNUM name=$NAME comments=$COMMENTS
+oneCmd.py iic scienceTrace halogen=30 arm=b specNum=$SPECNUM name=$NAME
 echo
 oneCmd.py iic bias specNum=$SPECNUM name=$NAME comments=$COMMENTS
+echo
+oneCmd.py iic scienceTrace halogen=3 arm=r specNum=$SPECNUM name=$NAME
+echo
+oneCmd.py iic scienceArc argon=4 specNum=$SPECNUM name=$NAME
+echo
+oneCmd.py iic scienceArc neon=3 specNum=$SPECNUM name=$NAME
+echo
+oneCmd.py iic scienceArc hgar=4 specNum=$SPECNUM name=$NAME
+echo
+oneCmd.py iic scienceArc krypton=60 specNum=$SPECNUM name=$NAME
+echo
+oneCmd.py iic bias specNum=$SPECNUM name=$NAME
 echo
