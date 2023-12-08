5e77e2ee1e36fd1b064011ac3c4ff434e9eb9f5f
Chi-Hung Yan <chyan@asiaa.sinica.edu.tw>
2023-12-07 23:52:41 -1000

    Fixing small bugs and tested

--- scripts/publishCobraGeometry.sh
+++ scripts/publishCobraGeometry.sh
@@ -15,7 +15,7 @@ if [ ! -e "$file" ]; then
   exit 1
 fi
 
-PFS_INSTDATA_DIR="/software/devel/chyan/pfs_instdata/data/pfi/modules/ALL/"
+PFS_INSTDATA_DIR="/software/devel/chyan/pfs_instdata"
 
 # Check if the destination directory exists
 if [ ! -d "$PFS_INSTDATA_DIR" ]; then
@@ -25,15 +25,27 @@ fi
 
 # Step 1: Copy the file to the destination directory
 echo "cp '$file' '$PFS_INSTDATA_DIR/data/pfi/modules/ALL/'"
+cp $file $PFS_INSTDATA_DIR/data/pfi/modules/ALL/
 
 # Step 2: Go to the directory
 echo "cd '$PFS_INSTDATA_DIR/data/pfi/modules/ALL/'"
+cd $PFS_INSTDATA_DIR/data/pfi/modules/ALL/
 
 # Step 3: Git add the copied file
 echo "git add $file"
+git add $file
 
 # Step 4: Create a symbolic link
 echo "ln -s $file ALL.xml"
+ln -sf $file ALL.xml
+git add ALL.xml
+
 
 # Step 5: Commit changes with a meaningful comment
 echo "git commit -m 'Publishing new geometry file'"
+git commit -m 'Publishing new geometry file'
+git push
+
+# Step 6: Ingest to database 
+echo "$PFS_UTILS_DIR/python/pfs/utils/ingestCobraGeometry.py $file"
+python $PFS_UTILS_DIR/python/pfs/utils/ingestCobraGeometry.py $file
